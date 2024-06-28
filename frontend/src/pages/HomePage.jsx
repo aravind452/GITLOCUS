a4bcc2c9eca44159652c939b81986fdf5efbcea8
aravind452 <aravindaru452@gmail.com>
2024-06-28 13:58:23 +0530

    frontend completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Search from "../components/Search";
+import Repos from "../components/Repos";
+import SortRepos from "../components/SortRepos";
+import ProfileInfo from "../components/ProfileInfo";
+import Spinner from "../components/Spinner";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className=" m-4 border-2 text-center p-2">
+      HomePage
+      <Search />
+      <SortRepos />
+      <div className="flex flex-col lg:flex-row justify-center items-start">
+        <ProfileInfo />
+        <Repos />
+        <Spinner/>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
