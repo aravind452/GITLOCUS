d828affe1837f0d6519c9ffe0709e9986773708d
aravind452 <aravindaru452@gmail.com>
2024-06-30 19:12:58 +0530

    serverjs updated

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,12 @@ import connectDB from "./db/connect.js";
 import "./passport/authentication.js";
 import session from "express-session";
 import passport from "passport";
-
+import path from "path";
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+console.log("dirname", __dirname);
 // should be placed on the top
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -22,7 +25,12 @@ app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/explore", exploreRouter);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on https://localhost:${PORT}`);
   connectDB();
 });
